12f3788be790f53f3f2852f08c077a5fdfeb2ee8
Mutombe <simbarashemutombe1@gmail.com>
2024-11-18 17:33:27 +0200

    ...

--- website/src/App.jsx
+++ website/src/App.jsx
@@ -1,45 +1,57 @@
-import React from 'react';
-import { ToastContainer } from 'react-toastify';
-import { Routes, Route } from 'react-router-dom';
-import 'react-toastify/dist/ReactToastify.css';
-import Navbar from './components/navbar/navbar';
-import Footer from './components/footer/footer';
-import HeroSection from './components/hero/hero';
-import { PortfolioPage } from './components/portfolio/portfolio';
-import PortfolioSection from './components/portfolio/portfolio';
-import { ServicesPage } from './components/services/services';
-import ServicesSection from './components/services/services';
-import { AboutPage } from './components/about/about';
-import AboutSection from './components/about/about';
-import { ContactPage } from './components/contact/contact';
-import ContactSection from './components/contact/contact';
+import React from "react";
+import { ToastContainer } from "react-toastify";
+import { Routes, Route, useLocation } from "react-router-dom";
+import "react-toastify/dist/ReactToastify.css";
+import Navbar from "./components/navbar/navbar";
+import Footer from "./components/footer/footer";
+import HeroSection from "./components/hero/hero";
+import { PortfolioPage } from "./components/portfolio/portfolio";
+import PortfolioSection from "./components/portfolio/portfolio";
+import { ServicesPage } from "./components/services/services";
+import ServicesSection from "./components/services/services";
+import { AboutPage } from "./components/about/about";
+import AboutSection from "./components/about/about";
+import { ContactPage } from "./components/contact/contact";
+import ContactSection from "./components/contact/contact";
 
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
 
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+};
 
 const ZettabyteWebsite = () => {
   return (
-<div className="flex flex-col min-h-screen">
-          <ToastContainer />
-          <Navbar />
-          <main className="flex-1">
-            <Routes>
-              <Route path="/" element={
-                <>
-                  <HeroSection />
-                  <ServicesSection />
-                  <PortfolioSection />
-                  <AboutSection />
-                  <ContactSection />
-                </>
-              } />
-              <Route path="/about" element={<AboutPage />} />
-              <Route path="/services" element={<ServicesPage />} />
-              <Route path="/portfolio" element={<PortfolioPage />} />
-              <Route path="/contact" element={<ContactPage />} />
-            </Routes>
-          </main>
-          <Footer />
-        </div>
+    <div className="flex flex-col min-h-screen">
+      <ToastContainer />
+      <ScrollToTop />
+      <Navbar />
+      <main className="flex-1">
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <HeroSection />
+                <ServicesSection />
+                <PortfolioSection />
+                <AboutSection />
+                <ContactSection />
+              </>
+            }
+          />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/services" element={<ServicesPage />} />
+          <Route path="/portfolio" element={<PortfolioPage />} />
+          <Route path="/contact" element={<ContactPage />} />
+        </Routes>
+      </main>
+      <Footer />
+    </div>
   );
 };
 
